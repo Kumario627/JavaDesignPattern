1fdaee109fc2031836bdba148cba32ecc6d76f7b
kumario <kumalog@jackumagic.com>
2022-07-02 01:57:49 +0900

    singletonパターンを生成

---
+++ src/singleton/Main.java
@@ -0,0 +1,19 @@
+package singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start.");
+
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+
+        if(obj1 == obj2) {
+            System.out.println("同一インスタンス");
+        } else {
+            System.out.println("異なるインスタンス");
+        }
+
+        System.out.println("end.");
+
+    }
+}
